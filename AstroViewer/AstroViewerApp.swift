2ebf483dd52d1a1e370cd81768c0d63aac23de40
Kyle Rohr <kyle@antipex.com>
2023-11-27 12:49:58 -0800

    Initial Commit

---
+++ AstroViewer/AstroViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstroViewerApp.swift
+//  AstroViewer
+//
+//  Created by Kyle Rohr on 11/27/23.
+//
+
+import SwiftUI
+
+@main
+struct AstroViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
